12a7e389d2f8d9d7e23248f00683c24554036ded
arundpm <arunraj.govindasamy@amwater.com>
2024-02-23 20:44:35 +0530

    Updated the string calc method

--- src/main/java/com/stringcalculator/StringUtil.java
+++ src/main/java/com/stringcalculator/StringUtil.java
@@ -1,13 +1,21 @@
 package com.stringcalculator;
 
+import java.util.Arrays;
+
 public class StringUtil {
 	
 	public static Integer stringCalculator(String input) {
 		
 		if(input.isEmpty())
 			return 0;
-
-        return Integer.parseInt(input);
+		
+		
+		String[] numsAsString = input.split(",");
+		
+        return  Arrays
+    	        .stream(numsAsString)
+    	        .mapToInt(Integer::parseInt)
+    	        .sum();
     }
 
 }
