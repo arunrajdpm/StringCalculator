874bb8b83dbf08c62e15f1b04502cea3be989df0
arundpm <arunraj.govindasamy@amwater.com>
2024-02-23 20:31:31 +0530

    updated string calc method

--- src/main/java/com/stringcalculator/StringUtil.java
+++ src/main/java/com/stringcalculator/StringUtil.java
@@ -3,6 +3,9 @@ package com.stringcalculator;
 public class StringUtil {
 	
 	public static Integer stringCalculator(String input) {
+		
+		if(input.isEmpty())
+			return 0;
 
         return Integer.parseInt(input);
     }
